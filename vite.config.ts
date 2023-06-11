95b6f6748ee59fd49c474c6092512193991ae88e
Sebastian Beltran <bjohansebas@gmail.com>
2023-06-11 15:38:05 -0500

    feat(Root): Add files path shortcut

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "components": path.resolve(__dirname, "src", "components"),
+      "hooks": path.resolve(__dirname, "src", "hooks"),
+      "services": path.resolve(__dirname, "src", "services"),
+      "assets": path.resolve(__dirname, "src", "assets"),
+      "context": path.resolve(__dirname, "src", "context"),
+      "utils": path.resolve(__dirname, "src", "utils"),
+    }
+  },
   plugins: [react(), svgr()],
 })
